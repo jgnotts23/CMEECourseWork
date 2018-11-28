96f5c237a022c5f68fe6227e66cda0ff6d7b11b0
jgnotts23 <jacob.griffiths18@imperial.ac.uk>
2018-11-28 16:31:21 +0000

    Added README file.

--- Week9/Code/jsg18.R
+++ Week9/Code/jsg18.R
@@ -27,25 +27,86 @@ choose_two <- function(x){
 }
 
 #5 - single step of neutral model sim
-neutral_step <- function(y){
-    x <- choose_two(length(y))
-    new <- replace(y, x[1], y[x[2]])
+neutral_step <- function(community){
+    x <- choose_two(length(community))
+    new <- replace(community, x[1], community[x[2]])
     return(new)
 }
 
 #6 - several steps
-neutral_generation <- function(a){
-    gentime <- ceiling(sum(a)/2)
+neutral_generation <- function(community){
+    gentime <- ceiling(length(community)/2)
     for (i in (1:gentime)){
-        a <- neutral_step(a)
+        community <- neutral_step(community)
     }
-return(a)
+return(community)
 }
 
 #7 - S
 neutral_time_series <- function(initial, duration){
+    rich <- species_richness(initial)
+    timeseries <- c(rich)
     for (i in (1:duration)){
         initial <- neutral_generation(initial)
-        print(initial)
+        rich <- species_richness(initial)
+        timeseries <- c(timeseries, rich)
     }
+    return(timeseries)
+}
+
+#8 - time series graph
+question_8 <- function(){
+    plot((neutral_time_series(initialise_max(100), duration=200)), type="l", xlab="Generations", ylab="Species Richness")
+}
+
+#9 - 
+neutral_step_speciation <- function(community, v){
+    p <- runif(1, 0, 1)
+    if (p < v){
+        neutral_step2(community)}
+    else {
+        neutral_step(community)}
+}
+
+neutral_step2 <- function(community){
+    x <- choose_two(length(community))
+    new <- replace(community, x[1], max(community)+10)
+    return(new)
+}
+
+#10 - 
+neutral_generation_speciation <- function(community, v){
+    gentime <- ceiling(length(community)/2)
+    for (i in (1:gentime)){
+        community <- neutral_step_speciation(community, v)
+    }
+return(community)
+}
+
+#11
+neutral_time_series_speciation <- function(community, v, duration){
+    rich <- species_richness(community)
+    timeseries <- c(rich)
+    for (i in (1:duration)){
+        community <- neutral_generation_speciation(community, v)
+        rich <- species_richness(community)
+        timeseries <- c(timeseries, rich)
+    }
+    return(timeseries)
+}
+
+#12
+question_12 <- function(){
+    plot((neutral_time_series_speciation(initialise_max(100), v=0.1, duration=200)), col="blue", type="l", xlab="Generations", ylab="Species Richness")
+    lines((neutral_time_series_speciation(initialise_max(100), v=0.1, duration=200)), col="red")
+}
+
+#13
+species_abundance <- function(community){
+    sort(as.numeric(table(community)), decreasing = TRUE)
+}
+
+#14
+octaves <- function(){
+    
 }
